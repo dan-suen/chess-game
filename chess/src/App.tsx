cb404a6b8cfa9e24baf219ac1b485156325dc3a5
dan-suen <danielsuen200791357@gmail.com>
2024-08-29 10:59:31 -0400

    finished createbaord function

--- chess/src/App.tsx
+++ chess/src/App.tsx
@@ -6,15 +6,16 @@ import createBoard from "./hooks/createBoard";
 import updateSentence from "./hooks/updateSentence";
 import updateTaken from "./hooks/updateTaken";
 function App() {
-  const [positions, setPositions] = useState<string[]>(Array(64).fill(null));
+  const [positions, setPositions] = useState<(string|null)[]>(Array(64).fill(null));
   const [flip, setFlip] = useState<boolean>(false);
   const [turn, setTurn] = useState<"black" | "white">("white");
   useEffect(() => {
-    createBoard();
+    createBoard(positions, flip);
     setTurn(flip ? "black" : "white")
   }, [flip]);
   useEffect(() => {
     addPieces();
+    setTurn(flip ? "black" : "white")
   }, [positions]);
   useEffect(() => {
     updateTaken();
@@ -26,9 +27,22 @@ function App() {
       <Toggle
         id="flip"
         defaultChecked={flip}
-        onChange={() => setFlip(!flip)}
+        onChange={() => {
+          setFlip(!flip)
+          setPositions([...positions].fill(null))
+        }}
       />
       <label htmlFor="flip">Play as Black</label>
+      <button onClick={() => {
+          setPositions([...positions].fill(null))
+        }}>New Game</button>
+      <table id="chessboard">
+
+
+
+
+
+      </table>
     </div>
   );
 }
--- chess/src/hooks/createBoard.tsx
+++ chess/src/hooks/createBoard.tsx
@@ -1,5 +1,37 @@
-const createBoard = function () {
+import React from "react";
 
+const createBoard = function (positions:(string|null)[], flip:boolean) {
+    let target = document.getElementById("chessboard");
+    if (target === null) {
+        console.error('Target element not found');
+        return;
+    }
+    if (target) {
+        target.innerHTML = '';
+    }
+    for (let i= 0 ; i< positions.length; i += 8 ) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < 8; j ++) {
+            let cell = document.createElement('th');
+            cell.classList.add(`${i+j}`);
+            if (!flip){
+
+                if ((i/8+j)%2 === 0) {
+                    cell.classList.add(`white`);
+                } else {
+                    cell.classList.add(`brown`);
+                }
+            } else {
+                if ((i/8+j)%2 === 0) {
+                    cell.classList.add(`brown`);
+                } else {
+                    cell.classList.add(`white`);
+                }
+            }
+            row.appendChild(cell);
+        }
+        target.appendChild(row);
+    }
 }
 
 export default createBoard;
